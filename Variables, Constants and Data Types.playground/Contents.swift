937d7edded843af19d6cdb9ae39e036b86435b19
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-11-23 21:29:04 -0800

    first commit

---
+++ Variables, Constants and Data Types.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var myAge : Int = 35
+
+myAge = 33
+
+let myName : String = "Julien"
+
+let myAgeInTenYears = myAge + 10
+
+let myFullName = "\(myName) Shim"
+
+let myDetails = "\(myFullName), \(myAge)"
+
+let wholeNumbers: Int = 12
+let text : String = "abc"
+let booleans: Bool = true
+let floatingPointNumber : Float = 1.3
+let double : Double = 3.1415965359
