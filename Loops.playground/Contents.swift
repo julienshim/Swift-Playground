4f5c3f6431144e16cdffcfb0e122be41c16fa7e8
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-11-24 12:46:32 -0800

    loops initial

---
+++ Loops.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+let arrayOfNumbers = [1,5,3,6,2,7,23,34]
+var sum = 0
+
+for number in arrayOfNumbers {
+    sum += number
+}
+
+print(sum)
+
+for number in 1 ... 10 where number % 3 == 0 {
+    print(number)
+}
