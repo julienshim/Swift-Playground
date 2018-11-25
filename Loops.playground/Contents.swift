e7807f215d47ac6de82b35f1ba3f3ce363179da7
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-11-24 21:43:42 -0800

    final beer song version

--- Loops.playground/Contents.swift
+++ Loops.playground/Contents.swift
@@ -17,13 +17,29 @@ func beerSong(forThisManyBottlesOfBeer totalNumberOfBottles: Int) -> String {
     var lyrics: String = ""
     for number in (0 ... totalNumberOfBottles).reversed() {
         if number > 1 {
-            lyrics += "\n\(number) bottles of beer on the wall, \(number) bottles of beer.\nTake one down and pass it around, \(number-1) bottles of beer on the wall.\n"
+            lyrics += """
+            \(number) bottles of beer on the wall, \(number) bottles of beer.
+            Take one down and pass it around, \(number-1) bottles of beer on the wall.
+            \n
+            """
         } else if number == 2 {
-            lyrics += "\n\(number) bottles of beer on the wall, \(number) bottles of beer.\nTake one down and pass it around, \(number-1) bottle of beer on the wall.\n"
+            lyrics += """
+            \(number) bottles of beer on the wall, \(number) bottles of beer.
+            Take one down and pass it around, \(number-1) bottle of beer on the wall.
+            \n
+            """
         } else if number == 1 {
-            lyrics += "\n\(number) bottle of beer on the wall, \(number) bottle of beer.\nTake one down and pass it around, \(number-1) bottles of beer on the wall.\n"
+            lyrics += """
+            \(number) bottle of beer on the wall, \(number) bottle of beer.
+            Take one down and pass it around, \(number-1) bottles of beer on the wall.
+            \n
+            """
         } else {
-            lyrics += "\nNo bottles of beer on the wall, No bottles of beer.\nGo to the store and buy some more, \(totalNumberOfBottles) bottles of beer on the wall.\n"
+            lyrics += """
+            No bottles of beer on the wall, no bottles of beer.
+            Go to the store and buy some more, \(totalNumberOfBottles) bottles of beer on the wall.
+            \n
+            """
         }
     }
     return lyrics
