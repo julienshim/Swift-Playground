3bc07b3b7093f8d092be76ff74e1f0127125c580
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-12-06 19:16:47 -0800

    typewriter playground

---
+++ Typewriter Effect.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+extension String {
+    var characterArray: [Character]{
+        var characterArray = [Character]()
+        for character in self {
+            characterArray.append(character)
+        }
+        return characterArray
+    }
+}
+
+extension UITextView {
+    func typeOn(string: String) {
+        let characterArray = string.characterArray
+        var characterIndex = 0
+        Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { (timer) in
+            if characterArray[characterIndex] != "$" {
+                while characterArray[characterIndex] == " " {
+                    self.text.append(" ")
+                    characterIndex += 1
+                    if characterIndex == characterArray.count {
+                        timer.invalidate()
+                        return
+                    }
+                }
+                self.text.append(characterArray[characterIndex])
+            }
+            characterIndex += 1
+            if characterIndex == characterArray.count {
+                timer.invalidate()
+            }
+        }
+    }
+}
+
+
+// Let's say you had an @IBOutlet weak var textView: UITextView!
+// Then you can let text = "I.$$$$$ Am.$$$$$$$$$$ Legend."
+// textView.typeOn(string: text)
+
+
