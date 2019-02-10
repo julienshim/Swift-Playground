6596b377824302b82635ed1d4fad02e94e972f35
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-02-10 13:36:09 -0800

    auto layout practice

---
+++ Auto-Layout-Practice/Auto Layout Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Auto Layout Practice
+//
+//  Created by Julien Shim on 01/31/2019.
+//  Copyright © 2019 Julien Shim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
