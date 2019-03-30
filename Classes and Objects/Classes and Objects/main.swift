661b7fb5528965e71b3fad8a209c573a878feaa5
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-03-30 09:17:25 -0700

    Classes and Objects init

---
+++ Classes and Objects/Classes and Objects/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Classes and Objects
+//
+//  Created by Julien Shim on 3/30/19.
+//  Copyright © 2019 Julien Shim. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
