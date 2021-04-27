663d0ce8942b249746bb61e101f09361bc50df59
julienshim <25723428+julienshim@users.noreply.github.com>
2021-04-27 13:03:51 -0700

    variables

---
+++ Coding Exercises/Variables.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+func exercise() {
+
+    var a = 5
+    var b = 8
+    
+    //Write your code here.
+    //Dont change any of the existing code.
+    let temp = a
+    a = b
+    b = temp
+
+
+    print("a: \(a)")
+    print("b: \(b)")
+    
+}
+
+exercise()
+
