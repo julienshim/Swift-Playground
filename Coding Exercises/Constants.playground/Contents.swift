0ee70e92d6682ab081f2f8c2cbfd4b95ff638436
julienshim <25723428+julienshim@users.noreply.github.com>
2021-04-27 13:02:48 -0700

    constants

---
+++ Coding Exercises/Constants.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+func exercise() {
+    //Write your code here.
+    let secondsInAnHour: Int = 60 * 60
+    
+    
+    //Don't change the code below.
+    print(secondsInAnHour)
+
+}
+
+exercise()
