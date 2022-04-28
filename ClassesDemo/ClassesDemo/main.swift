cbc2f56bafcd2d44fc842b8f8745e8bd31c8752d
julienshim <25723428+julienshim@users.noreply.github.com>
2022-04-27 19:03:08 -0700

    classesdemo

---
+++ ClassesDemo/ClassesDemo/main.swift
@@ -0,0 +1,23 @@
+
+//let skeleton = Enemy()
+
+let skeleton1 = Enemy(health: 100, attackStrength: 10)
+let skeleton2 = skeleton1
+
+skeleton1.takeDamage(amount: 10)
+print(skeleton2.health)
+
+//print(skeleton.health)
+//
+//skeleton.move()
+//skeleton.attack()
+//
+//let skeleton2 = Enemy()
+//let skeleton3 = Enemy()
+
+//let dragon = Dragon()
+//dragon.wingSpan = 5
+//dragon.attackStrength = 15
+//dragon.talk(speech: "My teeth are swords! My claws are spears! My wings are a hurricane")
+//dragon.move()
+//dragon.attack()
