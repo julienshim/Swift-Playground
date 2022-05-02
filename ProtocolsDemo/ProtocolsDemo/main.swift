83afaa0b9e09bf364974d11b78985bd36e8a5d9e
julienshim <25723428+julienshim@users.noreply.github.com>
2022-05-01 19:06:15 -0700

    prod

---
+++ ProtocolsDemo/ProtocolsDemo/main.swift
@@ -0,0 +1,69 @@
+protocol CanFly {
+    func fly()
+}
+
+
+class Bird {
+    
+    var isFemale = true
+    
+    func layEgg() {
+        if isFemale {
+            print("The bird makes a new bird in a shell.")
+        }
+    }
+}
+
+class Eagle: Bird, CanFly {
+    
+    func fly() {
+        print("The eagle flaps its wings and lifts off into the sky.")
+    }
+    
+    func soar(){
+        print("The eagle glides in the air using air currents.")
+    }
+}
+
+class Penguin: Bird {
+    func swim() {
+        print("The penguin paddles through the water.")
+    }
+}
+
+struct FlyingMuseum {
+    func flyingDemo(flyingObject: CanFly) {
+        flyingObject.fly()
+    }
+}
+
+//struct Airplane: {
+//
+//}
+
+struct AirPlane: CanFly {
+    func fly() {
+        print("The airplane uses its engine to lift off into the air.")
+    }
+}
+
+let myEagle = Eagle()
+
+//myEagle.fly()
+//myEagle.layEgg()
+//myEagle.soar()
+//
+let myPenguin = Penguin()
+//
+//myPenguin.layEgg()
+//myPenguin.swim()
+//myPenguin.fly()
+
+let myPlane = AirPlane()
+
+let museum = FlyingMuseum()
+museum.flyingDemo(flyingObject: myEagle)
+//museum.flyingDemo(flyingObject: myPenguin)
+//museum.flyingDemo(flyingObject: myPlane)
+
+
